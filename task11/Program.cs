6a64ed1ce75d299a7c460ab221980ee8c43067a2
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:48:53 +0300

    Добавьте файлы проекта.

---
+++ task11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив чисел
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Находим сумму элементов массива
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // Вычисляем среднее арифметическое значение элементов массива
+        double average = (double)sum / numbers.Length;
+
+        // Выводим результаты на экран
+        Console.WriteLine("Сумма элементов массива: " + sum);
+        Console.WriteLine("Среднее арифметическое значение элементов массива: " + average);
+    }
+}
